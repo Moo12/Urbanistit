97897670a8f31c240de59616d2a0509045a08ed8
Moo12 <moosesm@gmail.com>
2025-03-06 20:51:55 +0200

    wrapper upload images

---
+++ src/views/UploadImageWrapper.vue
@@ -0,0 +1,27 @@
+<template>
+  <form @submit.prevent="triggerUpload">
+    <UploadImagess ref="uploadImageRef" @file-submitted="handleFileSubmission" 
+          collection_name="general"   :dropdownOptions="['main', 'sub']"
+    />
+    <button>Submit from Parent</button>
+  </form>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  import UploadImagess from '@/components/UploadImages.vue'
+  
+  const uploadImageRef = ref(null);
+  
+  const triggerUpload = () => {
+    if (uploadImageRef.value) {
+      uploadImageRef.value.triggerSubmit(); // Calls the child's method
+    }
+  };
+
+  const handleFileSubmission = ({ role, files }) => {
+    console.log(`Role: ${role}`);
+    files.map(item => console.log(item))
+    };
+  </script>
+  
